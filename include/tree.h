bff1e6eceaeda790356ec5d60d84d92238cbca3f
kravchSof <kravch2202@gmail.com>
2026-05-30 20:24:45 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -8,49 +8,46 @@
 
 // Узел дерева перестановок
 struct TreeNode {
-    char value;  // Значение в узле
-    std::vector<std::shared_ptr<TreeNode>> children;  // Дочерние узлы
-    
-    TreeNode(char val) : value(val) {}
+    char value;
+    std::vector<std::shared_ptr<TreeNode>> children;
+
+    explicit TreeNode(char val) : value(val) {}
 };
 
 class PMTree {
-private:
+ private:
     std::shared_ptr<TreeNode> root;
     std::vector<char> elements;
-    
-    // Рекурсивное построение дерева
-    void buildTree(std::shared_ptr<TreeNode> node, std::vector<char> remaining);
-    
-    // Рекурсивный сбор всех перестановок
-    void collectPerms(std::shared_ptr<TreeNode> node, std::vector<char>& current, 
+
+    void buildTree(std::shared_ptr<TreeNode> node,
+                   std::vector<char> remaining);
+
+    void collectPerms(std::shared_ptr<TreeNode> node,
+                     std::vector<char>& current,
                      std::vector<std::vector<char>>& result);
-    
-    // Рекурсивный поиск перестановки по номеру (метод обхода)
-    bool findPermByNum(std::shared_ptr<TreeNode> node, std::vector<char>& current,
-                      int& counter, int targetNum, std::vector<char>& result);
-    
-    // Рекурсивный поиск перестановки по номеру (метод навигации)
-    bool navigateToPerm(std::shared_ptr<TreeNode> node, std::vector<char>& current,
-                        int targetNum, std::vector<char>& result, int& index);
-    
-public:
-    // Конструктор
-    PMTree(const std::vector<char>& input);
-    
-    // Получение всех перестановок
+
+    bool findPermByNum(std::shared_ptr<TreeNode> node,
+                      std::vector<char>& current,
+                      int& counter, int targetNum,
+                      std::vector<char>& result);
+
+    bool navigateToPerm(std::shared_ptr<TreeNode> node,
+                       std::vector<char>& current,
+                       int targetNum, std::vector<char>& result,
+                       int& index);
+
+ public:
+    explicit PMTree(const std::vector<char>& input);
+
     std::vector<std::vector<char>> getAllPerms();
-    
-    // Дружественные функции для доступа к private методам
+
     friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
     friend std::vector<char> getPerm1(PMTree& tree, int num);
     friend std::vector<char> getPerm2(PMTree& tree, int num);
-    
-    // Метод для получения корня дерева (для friend функций)
+
     std::shared_ptr<TreeNode> getRoot() const { return root; }
 };
 
-// Внешние функции
 std::vector<std::vector<char>> getAllPerms(PMTree& tree);
 std::vector<char> getPerm1(PMTree& tree, int num);
 std::vector<char> getPerm2(PMTree& tree, int num);
