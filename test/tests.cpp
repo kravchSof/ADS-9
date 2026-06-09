67d537831baba8339f2eb66d22ebbd6fc0fb3b84
kravchSof <kravch2202@gmail.com>
2026-06-09 10:16:48 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,30 +1,33 @@
-// Copyright 2025 NNTU-CS
+// Copyright 2020 GHA
 #include <gtest/gtest.h>
-#include <string>
 #include <vector>
-
-#include "tree.h"
+#include "../include/tree.h"
 
 TEST(ads9, test1) {
-  PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm1(tree, 1);
-  ASSERT_TRUE(result[0] == '1' && result[1] == '2' && result[2] == '3');
+    PMTree tree(std::vector<char>{'1', '2', '3'});
+    std::vector<char> result = getPerm1(tree, 1);
+    ASSERT_TRUE(result[0] == '1' && result[1] == '2' && result[2] == '3');
 }
 
 TEST(ads9, test2) {
-  PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm2(tree, 2);
-  ASSERT_TRUE(result[0] == '1' && result[1] == '3' && result[2] == '2');
+    PMTree tree(std::vector<char>{'1', '2', '3'});
+    std::vector<char> result = getPerm2(tree, 2);
+    ASSERT_TRUE(result[0] == '1' && result[1] == '3' && result[2] == '2');
 }
 
 TEST(ads9, test3) {
-  PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm1(tree, 6);
-  ASSERT_TRUE(result[0] == '3' && result[1] == '2' && result[2] == '1');
+    PMTree tree(std::vector<char>{'1', '2', '3'});
+    std::vector<char> result = getPerm1(tree, 6);
+    ASSERT_TRUE(result[0] == '3' && result[1] == '2' && result[2] == '1');
 }
 
 TEST(ads9, test4) {
-  PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm2(tree, 8);
-  ASSERT_EQ(result.size(), 0);
+    PMTree tree(std::vector<char>{'1', '2', '3'});
+    std::vector<char> result = getPerm2(tree, 8);
+    ASSERT_EQ(result.size(), 0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
