331eacb9a251e4e91050cab7ff7db94905761754
kravchSof <kravch2202@gmail.com>
2026-06-09 10:51:27 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -3,45 +3,33 @@
 #define INCLUDE_TREE_H_
 
 #include <vector>
-#include <memory>
 
-// Узел дерева
 struct Node {
     char value;
-    std::vector<std::shared_ptr<Node>> children;
+    std::vector<Node*> children;
+    size_t subtree_size;
 
-    explicit Node(char val) : value(val) {}
+    explicit Node(char v);
+    ~Node();
 };
 
-// Класс дерева перестановок
 class PMTree {
  private:
-    std::shared_ptr<Node> root;
-    int size;
+    Node* root;
 
-    void buildTree(std::shared_ptr<Node> node, std::vector<char> remaining);
-    void collectPerms(std::shared_ptr<Node> node,
-                      std::vector<char>& current,
-                      std::vector<std::vector<char>>& result) const;
-    int getSubtreePermCount(std::shared_ptr<Node> node, int depth) const;
+    std::vector<Node*> buildChildren(std::vector<char> available);
+    void collectPerms(Node* node, std::vector<char>& current,
+                      std::vector<std::vector<char>>& output) const;
 
  public:
     explicit PMTree(const std::vector<char>& symbols);
+    ~PMTree();
 
-    std::shared_ptr<Node> getRoot() const { return root; }
-    int getSize() const { return size; }
-
-    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
-    friend std::vector<char> getPerm1(PMTree& tree, int num);
-    friend std::vector<char> getPerm2(PMTree& tree, int num);
+    Node* getRoot() const;
+    std::vector<std::vector<char>> getAllPerms() const;
 };
 
-// Функции для работы с деревом
-std::vector<std::vector<char>> getAllPerms(PMTree& tree);
 std::vector<char> getPerm1(PMTree& tree, int num);
 std::vector<char> getPerm2(PMTree& tree, int num);
-
-// Вспомогательная функция
-int factorial(int n);
 
 #endif  // INCLUDE_TREE_H_
